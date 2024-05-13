d3e417ddc4391e516e58de61dfbc1138ad1dd845
psm <psmin9325@gmail.com>
2024-05-13 15:27:15 +0900

    feat: rendering home file with datas

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,14 +1,22 @@
 const asyncHandler = require("express-async-handler");
 const nologinlayout = "./layouts/noLoginLayout";
 const loginlayout = "./loginUsers/loginLayout";
+const Post = require("../models/postModel");
 
 //@desc view homePage
 //@route GET /
-const homePage = (req, res) => {
+const homePage = asyncHandler(async(req, res) => {
     const locals = {
         title : "Home",
-    }
-    res.render("home", { locals, layout : loginlayout });
-};
+    };
+
+    const posts = await Post.find({}).sort({ updatedAt : "desc" }).limit(10); // updatedAt : -1 도 같은 결과를 낸다.
+
+    res.render("home", { locals, posts, layout : nologinlayout });
+});
+
+const recentPost = asyncHandler(async (req, res) => {
+
+})
 
 module.exports = { homePage };
